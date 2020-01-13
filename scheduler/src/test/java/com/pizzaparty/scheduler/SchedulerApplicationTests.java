b90e603c2b1c7dabb982c07fdeccfe70d414a2f0
Adam Publicover <adam.publicover@georgebrown.ca>
2020-01-13 14:35:56 -0500

    initial upload of Spring Initializr project

---
+++ scheduler/src/test/java/com/pizzaparty/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzaparty.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
