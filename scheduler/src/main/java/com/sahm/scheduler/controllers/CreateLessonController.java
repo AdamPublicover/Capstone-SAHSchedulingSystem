099a6c3179ab3dbb50f4b9c493b3bf40b890ff97
Adam Publicover <adam.publicover@georgebrown.ca>
2020-03-02 15:37:49 -0500

    fixed project ssetup to no longer require javax.json lib

--- scheduler/src/main/java/com/sahm/scheduler/controllers/CreateLessonController.java
+++ scheduler/src/main/java/com/sahm/scheduler/controllers/CreateLessonController.java
@@ -20,7 +20,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import javax.json.*;
 
 @Controller
 public class CreateLessonController {
