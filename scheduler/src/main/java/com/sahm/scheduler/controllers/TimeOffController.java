9c7e9288144366611009fcc8479a4b771ed0cc47
ejayk <estherkim0907@gmail.com>
2020-03-17 02:08:36 -0400

    set avail, time off teachers view

---
+++ scheduler/src/main/java/com/sahm/scheduler/controllers/TimeOffController.java
@@ -0,0 +1,30 @@
+package com.sahm.scheduler.controllers;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+import javax.servlet.http.*;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TimeOffController {
+	@RequestMapping(value="/time_off_t")
+	public String hello(Model model) {
+		return "time_off_t";
+	}
+}
